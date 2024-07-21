0699c750d39e57c6a96d924b26cefc900f37abb6
MathScopim <matheus.scopim@gmail.com>
2024-07-21 20:36:55 -0300

    Estudo de um set de produtos

---
+++ src/CadastroProdutos.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    //atributos
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(nome, cod, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1L, "Leite", 7.33, 15);
+        cadastroProdutos.adicionarProduto(3L, "Fandangos", 8.44, 3);
+        cadastroProdutos.adicionarProduto(8L, "Macarrão", 14.2, 44);
+        cadastroProdutos.adicionarProduto(5L, "Farinha", 8.37, 20);
+        cadastroProdutos.adicionarProduto(1L, "Tempero", 13.88, 17);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
